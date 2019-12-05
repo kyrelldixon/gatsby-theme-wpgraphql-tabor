c0c9470a6dba5efd5c015dc34e545e6370b29a97
Kyrell Dixon <kyrell@kyrelldixon.com>
2019-12-05 15:35:41 -0600

    Fix social icon url and open in new tab

--- packages/gatsby-theme-wpgraphql-tabor/src/components/SocialNav.js
+++ packages/gatsby-theme-wpgraphql-tabor/src/components/SocialNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Dribble, Twitter, Medium } from './Icons';
+import { Dribble, Twitter } from './Icons';
 //TODO: make it dynamic
+//TODO: add facebook and instagram icons
 
 const SocialNav = () => (
   <nav className="social-navigation" aria-label="Social Menu">
@@ -13,7 +14,7 @@ const SocialNav = () => (
           id="menu-item-351"
           className="menu-item menu-item-type-custom menu-item-object-custom menu-item-351"
         >
-          <a href="http://twitter.com/richard_tabor">
+          <a href="https://twitter.com/kyrelldixon" target="_blank" rel="noopener noreferrer">
             <span className="screen-reader-text">Twitter</span>
             <Twitter />
           </a>
@@ -22,18 +23,9 @@ const SocialNav = () => (
           id="menu-item-352"
           className="menu-item menu-item-type-custom menu-item-object-custom menu-item-352"
         >
-          <a href="http://dribbble.com/richtabor">
-            <span className="screen-reader-text">Dribbble</span>
+          <a href="https://instagram.com/kyrell.dixon" target="_blank" rel="noopener noreferrer">
+            <span className="screen-reader-text">Instagram</span>
             <Dribble />
-          </a>
-        </li>
-        <li
-          id="menu-item-353"
-          className="menu-item menu-item-type-custom menu-item-object-custom menu-item-353"
-        >
-          <a href="https://medium.com/@richtabor">
-            <span className="screen-reader-text">Medium</span>
-            <Medium />
           </a>
         </li>
       </ul>
